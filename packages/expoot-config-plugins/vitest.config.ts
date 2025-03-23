3b0f3daad68c0956487ec62d94f34bff68b864e7
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-03-23 13:17:16 +0900

    mock 'xcode' module and stop using deprecated 'deps' field

--- packages/expoot-config-plugins/vitest.config.ts
+++ packages/expoot-config-plugins/vitest.config.ts
@@ -1,5 +1,6 @@
 import * as path from 'node:path';
 
+import commonjs from 'vite-plugin-commonjs';
 import { defineConfig } from 'vitest/config';
 
 const workspace = import.meta.dirname;
@@ -9,6 +10,17 @@ const monorepoRoot = path.resolve(packages, '..');
 const rootNodeModules = path.resolve(monorepoRoot, 'node_modules');
 
 export default defineConfig({
+  plugins: [
+    commonjs({
+      filter(id) {
+        // `node_modules` is exclude by default, so we need to include it explicitly
+        // https://github.com/vite-plugin/vite-plugin-commonjs/blob/v0.7.0/src/index.ts#L125-L127
+        if (id.includes('node_modules/xcode')) {
+          return true;
+        }
+      },
+    }),
+  ],
   test: {
     root: workspace,
     name: '@expoot/config-plugins',
@@ -17,11 +29,15 @@ export default defineConfig({
     clearMocks: true,
     globals: true,
     silent: false,
-    deps: {
-      inline: ['@expo/config-plugins', 'glob'],
-      moduleDirectories: [packages, rootNodeModules, workspaceNodeModules].map(
-        (x) => path.relative(workspace, x)
-      ),
+    server: {
+      deps: {
+        inline: ['@expo/config-plugins', 'glob', 'xcode'],
+        moduleDirectories: [
+          packages,
+          rootNodeModules,
+          workspaceNodeModules,
+        ].map((x) => path.relative(workspace, x)),
+      },
     },
   },
 });
