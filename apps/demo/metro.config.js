7327b3b5e518bfe856eacc26e9a8e8e488deda62
shirakaba <14055146+shirakaba@users.noreply.github.com>
2026-05-07 22:58:41 +0900

    stop spammy log

--- apps/demo/metro.config.js
+++ apps/demo/metro.config.js
@@ -7,7 +7,7 @@ module.exports = makeMetroConfig({
   serializer: {
     ...defaultConfig.serializer,
     getPolyfills: () => {
-      console.log("getPolyfills()");
+      // console.log("getPolyfills()");
       const polyfills = require("@react-native/js-polyfills")();
       polyfills.push(require.resolve("./expo.polyfill.js"));
       return polyfills;
