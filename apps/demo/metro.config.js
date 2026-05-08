bfaf219949e1b7fcd351e6970a75b4d02a3ab790
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2026-05-08 16:19:25 +0900

    update create-expo-desktop-app logic

--- apps/demo/metro.config.js
+++ apps/demo/metro.config.js
@@ -1,16 +1,16 @@
 const { getDefaultConfig } = require("@expo/metro-config");
 const { makeMetroConfig } = require("@rnx-kit/metro-config");
 
-const defaultConfig = getDefaultConfig(__dirname);
-module.exports = makeMetroConfig({
-  ...defaultConfig,
-  serializer: {
-    ...defaultConfig.serializer,
-    getPolyfills: () => {
-      // console.log("getPolyfills()");
-      const polyfills = require("@react-native/js-polyfills")();
-      polyfills.push(require.resolve("./expo.polyfill.js"));
-      return polyfills;
-    },
-  },
-});
+const config = makeMetroConfig(getDefaultConfig(__dirname));
+
+const getPolyfills = config.serializer.getPolyfills;
+const windowsExpoPolyfill = require.resolve("./expo-polyfill.windows.js");
+config.serializer.getPolyfills = (platform) => {
+  const polyfills = getPolyfills(platform);
+  if (platform === "windows") {
+    polyfills.push(windowsExpoPolyfill);
+  }
+  return polyfills;
+};
+
+module.exports = config;
--- packages/expo-desktop/src/create-app/create-expo-desktop-app.ts
+++ packages/expo-desktop/src/create-app/create-expo-desktop-app.ts
@@ -139,11 +139,10 @@ export async function createExpoDesktopApp({
 
   title("Adding Expo support to the Metro config…", { spacing: 1 });
   await improveMetroConfig({ projectPath });
+  await addWindowsExpoPolyfill({ projectPath });
 
   title("Adding Expo support to the Babel config…", { spacing: 1 });
   await writeBabelConfig({ projectPath });
-
-  // TODO: Set up Windows app.cpp entrypoint
 }
 
 async function createExpoApp({
@@ -380,13 +379,15 @@ async function updatePackageJson({
       packageJson.dependencies = {};
     }
 
-    // expo-desktop-prebuild-config itself depends on
-    // expo-desktop-config-plugins.
-    if (localDev) {
-      packageJson.dependencies["expo-desktop-prebuild-config"] =
-        "file:../../expo-desktop-prebuild-config";
-    } else {
-      packageJson.dependencies["expo-desktop-prebuild-config"] = "^1.0.0";
+    const monorepoDeps = {
+      // expo-desktop-prebuild-config itself depends on
+      // expo-desktop-config-plugins.
+      ["expo-desktop-prebuild-config"]: "^1.0.0",
+      ["expo-desktop-modules-core"]: `^${versions.expoMajor}.0.0`,
+      ["expo-desktop-stubs"]: `^${versions.expoMajor}.0.0`,
+    };
+    for (const [key, value] of Object.entries(monorepoDeps)) {
+      packageJson.dependencies[key] = localDev ? `file:../../${key}` : value;
     }
 
     packageJson.dependencies["react-native-macos"] = versions.macos;
@@ -615,6 +616,17 @@ const { getDefaultConfig } = require("@expo/metro-config");
 const { makeMetroConfig } = require("@rnx-kit/metro-config");
 
 const config = makeMetroConfig(getDefaultConfig(__dirname));
+
+const getPolyfills = config.serializer.getPolyfills;
+const windowsExpoPolyfill = require.resolve("./expo-polyfill.windows.js");
+config.serializer.getPolyfills = (platform) => {
+  const polyfills = getPolyfills(platform);
+  if (platform === "windows") {
+    polyfills.push(windowsExpoPolyfill);
+  }
+  return polyfills;
+};
+
 module.exports = config;
     `.trim() + "\n",
       "utf-8",
@@ -627,6 +639,74 @@ module.exports = config;
   }
 
   console.log(`\n${green("◆")}  Overwrote metro.config.js.\n`);
+}
+
+/**
+ */
+async function addWindowsExpoPolyfill({ projectPath }: { projectPath: string }) {
+  await fs.writeFile(
+    path.resolve(projectPath, "expo-polyfill.windows.js"),
+    `
+try {
+  // Until we can configure TurboModules for eager initialisation (which is
+  // waiting on https://github.com/microsoft/react-native-windows/pull/16093), we
+  // need to trigger the lazy-init of our TurboModule by accessing it for the
+  // first time, which causes the TurboModuleManager to call its REACT_INIT
+  // method.
+  //
+  // We can't do any imports inside getPolyfills(), but can use the global proxy:
+  globalThis.nativeModuleProxy.ExpoMainRuntimeInstaller;
+
+  // TODO: Implement Expo's NativeModulesProxy and make all of this
+  //       lazy-initialised (and actually populate \`exportedMethods\`, etc.).
+
+  // Below are temporary stubs to suppress these warnings:
+  //  WARN  The "EXNativeModulesProxy" native module is not exported through NativeModules; verify that expo-modules-core's native code is linked properly
+  //  WARN  No native ExponentConstants module found, are you sure the expo-constants's module is linked properly?
+  //  WARN  No native ExponentConstants module found, are you sure the expo-constants's module is linked properly?
+
+  // Funnily enough, although expo-modules-core prefers to access
+  // global.expo?.modules?.NativeModulesProxy over the deprecated
+  // NativeModules?.NativeUnimoduleProxy, we still need the latter to exist in
+  // order for it to check the former. And it's only
+  globalThis.nativeModuleProxy.NativeUnimoduleProxy;
+  const { ExpoAsset } = globalThis.nativeModuleProxy;
+
+  const ExponentConstants = {};
+
+  globalThis.expo.modules = {
+    ExpoAsset,
+    ExponentConstants,
+    // - JS: apps/demo/node_modules/expo-modules-core/src/NativeModulesProxy.native.ts
+    // - iOS:
+    //   - NativeUnimoduleProxy: apps/demo/node_modules/expo-modules-core/ios/Legacy/NativeModulesProxy/NativeModulesProxyModule.swift
+    //   - NativeModulesProxy: apps/demo/node_modules/expo-modules-core/ios/Legacy/NativeModulesProxy/EXNativeModulesProxy.mm
+    // - Android:
+    //   - NativeUnimoduleProxy: apps/demo/node_modules/expo-modules-core/android/src/main/java/expo/modules/adapters/react/NativeModulesProxy.java
+    //   - NativeModulesProxy: apps/demo/node_modules/expo-modules-core/android/src/main/java/expo/modules/kotlin/defaultmodules/NativeModulesProxyModule.kt
+    NativeModulesProxy: {
+      exportedMethods: {
+        ExpoAsset: [],
+        ExponentConstants: [],
+      },
+      modulesConstants: {
+        ExpoAsset: [],
+        ExponentConstants: [],
+      },
+    },
+  };
+
+  // Now expo-modules-core can populate its fileprivate
+  // \`const NativeModulesProxy: Record<string, ProxyNativeModule = {}\` from our
+  // global.expo?.modules?.NativeModulesProxy. This avoids the warning about
+  // EXNativeModulesProxy being missing.
+} catch (error) {
+  console.error("Polyfill failed", error);
+  throw error;
+}
+    `.trim() + "\n",
+    "utf-8",
+  );
 }
 
 async function updatePodfile({ projectPath }: { projectPath: string }) {
